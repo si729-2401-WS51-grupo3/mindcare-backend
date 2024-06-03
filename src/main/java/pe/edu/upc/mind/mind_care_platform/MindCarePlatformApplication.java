23550bbb4f62d82f95843bf7b40303a3939de144
aksoonie <u202210778@upc.edu.pe>
2024-06-02 20:28:30 -0500

    fixing

--- src/main/java/pe/edu/upc/mind/mind_care_platform/MindCarePlatformApplication.java
+++ src/main/java/pe/edu/upc/mind/mind_care_platform/MindCarePlatformApplication.java
@@ -1,13 +1,12 @@
 package pe.edu.upc.mind.mind_care_platform;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MindCarePlatformApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(MindCarePlatformApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(MindCarePlatformApplication.class, args);
+	}
 
 }
