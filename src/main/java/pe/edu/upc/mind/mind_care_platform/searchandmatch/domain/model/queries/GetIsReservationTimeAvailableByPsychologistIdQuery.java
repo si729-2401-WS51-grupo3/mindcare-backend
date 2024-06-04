585dcfc0ba382738ce526a6d46e9e3f85049e0c9
aksoonie <u202210778@upc.edu.pe>
2024-06-03 19:47:59 -0500

    feat: refactor GetIsReservationTimeAvailableByIdQuery to GetIsReservationTimeAvailableByPsychologistIdQuery

--- src/main/java/pe/edu/upc/mind/mind_care_platform/searchandmatch/domain/model/queries/GetIsReservationTimeAvailableByPsychologistIdQuery.java
+++ src/main/java/pe/edu/upc/mind/mind_care_platform/searchandmatch/domain/model/queries/GetIsReservationTimeAvailableByPsychologistIdQuery.java
@@ -2,5 +2,5 @@ package pe.edu.upc.mind.mind_care_platform.searchandmatch.domain.model.queries;
 
 import pe.edu.upc.mind.mind_care_platform.searchandmatch.domain.model.valueobjects.PsychologistId;
 
-public record GetPsychologistScheduleByPsychologistIdQuery(PsychologistId psychologistId) {
+public record GetIsReservationTimeAvailableByPsychologistIdQuery(PsychologistId psychologistId) {
 }
