b6d5c9b574402cd8cd91493ca05e45043726036e
Sebastian Cachis <u202210846@upc.edu.pe>
2024-06-09 15:16:07 -0500

    fix: appointment managment model and shared structure

--- src/main/java/pe/edu/upc/mind/mind_care_platform/appointmentManagement/interfaces/acl/AppointmentsContextFacade.java
+++
@@ -1,2 +0,0 @@
-package pe.edu.upc.mind.mind_care_platform.appointmentManagement.interfaces.acl;public class AppointmentsContextFacade {
-}
--- src/main/java/pe/edu/upc/mind/mind_care_platform/appointmentManagement/interfaces/rest/resources/CreateAppointmentResource.java
+++
@@ -1,2 +0,0 @@
-package pe.edu.upc.mind.mind_care_platform.appointmentManagement.interfaces.rest.resources;public record CreateAppointmentResource() {
-}
