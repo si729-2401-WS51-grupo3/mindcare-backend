8c19c57e2c4e75fae91a5e0e33691203917efe0f
EliasYP <u202211817@upc.edu.pe>
2024-06-09 22:51:30 -0500

    fix: change the openapi configuration to deploy the swagger

--- src/main/java/pe/edu/upc/mind/mind_care_platform/NoteManagement/domain/model/entities/Note.java
+++ src/main/java/pe/edu/upc/mind/mind_care_platform/NoteManagement/domain/model/entities/Note.java
@@ -4,10 +4,11 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 import pe.edu.upc.mind.mind_care_platform.NoteManagement.domain.model.aggregates.AppointmentNote;
+import pe.edu.upc.mind.mind_care_platform.shared.domain.model.entities.AuditableModel;
 
 @Getter
 @Entity
-public class Note {
+public class Note extends AuditableModel {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
